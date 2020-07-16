0fbd1358f8d18f52f1b5ff78c3406485a8285678
Arman9696 <68379598+Arman9696@users.noreply.github.com>
2020-07-16 17:06:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://allfont.ru/allfont.css?fonts=europe-normal" rel="stylesheet" type="text/css" />
+	<link href="http://allfont.ru/allfont.css?fonts=europe-normal" rel="stylesheet" type="text/css" />
 	<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
 
 	<link href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
